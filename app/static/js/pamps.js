58f371f0be5db4793799e5a201759083513f645d
g.e. link <link@grace.cat>
2015-05-26 15:44:32 -0700

    hit objects and they talk back

--- app/static/js/pamps.js
+++ app/static/js/pamps.js
@@ -5,10 +5,14 @@ $(function() {
   canvas.width = $(window).innerWidth() - (2 * border);
   canvas.height = $(window).innerHeight() - footer;
 
-  spritesToLoad = 1;
+  spritesToLoad = 2;
   mapSprite = new Image();
   mapSprite.src ='static/img/mapsprite.png';
   mapSprite.onload = spriteLoaded;
+
+  objectSprite = new Image();
+  objectSprite.src = 'static/img/objectsprite.png';
+  objectSprite.onload = spriteLoaded;
 
   mapWidth = 31;
   mapHeight = 19;
@@ -32,39 +36,75 @@ $(function() {
   };
 
   mapArray = [
-      [0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-      [1,1,1,1,2,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0],
-      [1,1,1,1,1,0,1,1,0,1,1,0,0,0,0,0,0,0,0,3],
-      [1,1,2,1,2,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0],
-      [1,1,1,1,1,1,1,0,1,2,2,1,2,0,0,0,0,3,4,4],
-      [1,1,1,1,0,1,1,1,1,2,0,0,0,2,0,0,0,3,4,4],
-      [1,0,0,1,1,1,1,2,2,1,0,0,0,0,0,0,3,0,4,4],
-      [0,0,0,1,1,1,1,1,2,1,0,0,0,0,0,0,3,0,3,3],
-      [0,0,0,0,0,2,2,1,1,1,0,0,0,0,0,0,4,3,3,0],
-      [0,0,0,0,0,0,2,0,1,0,0,0,0,0,0,3,3,4,3,0],
-      [0,0,0,0,0,0,0,2,2,0,0,0,0,0,3,3,4,4,3,0],
-      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,4,3,3,4,0],
-      [1,0,0,0,0,0,0,0,0,0,0,0,0,5,3,3,3,3,4,0],
-      [1,1,0,0,0,0,0,0,0,0,0,0,5,5,5,4,4,4,4,0],
-      [1,2,2,0,0,0,0,0,0,0,0,5,5,5,5,4,0,4,4,0],
-      [1,1,2,0,0,0,0,0,0,0,0,5,5,5,5,4,0,3,0,0],
-      [1,1,0,0,0,0,0,0,0,0,0,5,5,5,5,0,1,4,4,0],
-      [2,1,1,0,0,0,0,0,0,0,0,5,5,5,0,0,0,0,0,0],
-      [1,2,2,2,0,0,0,0,0,0,5,5,5,5,0,0,0,0,0,0],
-      [1,2,0,0,0,1,0,0,0,5,5,5,0,0,0,1,0,0,0,0],
-      [1,1,0,0,0,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0],
-      [2,1,1,0,0,1,2,1,1,0,0,1,0,0,0,0,0,0,0,0],
-      [1,0,1,1,0,1,1,0,1,1,0,1,1,0,0,0,0,0,0,0],
-      [2,1,1,0,0,2,2,1,1,0,0,1,1,1,1,0,0,0,0,0],
-      [1,1,1,0,1,2,1,1,1,0,1,2,2,1,2,0,0,0,0,4],
-      [0,1,1,1,1,1,0,1,1,1,1,2,0,0,0,2,0,0,0,4],
-      [1,1,1,2,2,1,1,1,1,2,2,1,0,0,0,0,0,0,0,0],
-      [1,1,1,1,0,1,1,1,1,1,2,1,0,0,0,0,0,0,0,0],
-      [0,2,2,1,1,0,0,2,2,1,1,1,0,0,0,0,0,0,0,3],
-      [0,0,2,0,1,0,0,0,2,0,1,0,0,0,0,0,0,3,4,4],
-      [1,2,2,1,1,0,0,2,1,2,1,1,0,0,0,0,0,0,0,3],
-      [1,1,2,0,1,0,0,0,0,0,1,0,0,0,0,0,0,3,3,3]
-    ];
+    [0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,2,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,0,1,1,0,1,1,0,0,0,0,0,0,0,0,3],
+    [1,1,2,1,2,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,0,1,2,2,1,2,0,0,0,0,3,4,4],
+    [1,1,1,1,0,1,1,1,1,2,0,0,0,2,0,0,0,3,4,4],
+    [1,0,0,1,1,1,1,2,2,1,0,0,0,0,0,0,3,0,4,4],
+    [0,0,0,1,1,1,1,1,2,1,0,0,0,0,0,0,3,0,3,3],
+    [0,0,0,0,0,2,2,1,1,1,0,0,0,0,0,0,4,3,3,0],
+    [0,0,0,0,0,0,2,0,1,0,0,0,0,0,0,3,3,4,3,0],
+    [0,0,0,0,0,0,0,2,2,0,0,0,0,0,3,3,4,4,3,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,4,3,3,4,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,5,3,3,3,3,4,0],
+    [1,1,0,0,0,0,0,0,0,0,0,0,5,5,5,4,4,4,4,0],
+    [1,2,2,0,0,0,0,0,0,0,0,5,5,5,5,4,0,4,4,0],
+    [1,1,2,0,0,0,0,0,0,0,0,5,5,5,5,4,0,3,0,0],
+    [1,1,0,0,0,0,0,0,0,0,0,5,5,5,5,0,1,4,4,0],
+    [2,1,1,0,0,0,0,0,0,0,0,5,5,5,0,0,0,0,0,0],
+    [1,2,2,2,0,0,0,0,0,0,5,5,5,5,0,0,0,0,0,0],
+    [1,2,0,0,0,1,0,0,0,5,5,5,0,0,0,1,0,0,0,0],
+    [1,1,0,0,0,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0],
+    [2,1,1,0,0,1,2,1,1,0,0,1,0,0,0,0,0,0,0,0],
+    [1,0,1,1,0,1,1,0,1,1,0,1,1,0,0,0,0,0,0,0],
+    [2,1,1,0,0,2,2,1,1,0,0,1,1,1,1,0,0,0,0,0],
+    [1,1,1,0,1,2,1,1,1,0,1,2,2,1,2,0,0,0,0,4],
+    [0,1,1,1,1,1,0,1,1,1,1,2,0,0,0,2,0,0,0,4],
+    [1,1,1,2,2,1,1,1,1,2,2,1,0,0,0,0,0,0,0,0],
+    [1,1,1,1,0,1,1,1,1,1,2,1,0,0,0,0,0,0,0,0],
+    [0,2,2,1,1,0,0,2,2,1,1,1,0,0,0,0,0,0,0,3],
+    [0,0,2,0,1,0,0,0,2,0,1,0,0,0,0,0,0,3,4,4],
+    [1,2,2,1,1,0,0,2,1,2,1,1,0,0,0,0,0,0,0,3],
+    [1,1,2,0,1,0,0,0,0,0,1,0,0,0,0,0,0,3,3,3]
+  ];
+
+  objectArray = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0],
+    [0,1,1,2,3,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  ];
+
 
   context = canvas.getContext('2d');
   function spriteLoaded() {
@@ -73,8 +113,7 @@ $(function() {
   }
 
   function run() {
-    pample.gameBoard.drawMap();
-    pample.gameBoard.drawPlayer();
+    pample.gameBoard.redraw();
   }
 
   var pample = pample || {};
@@ -83,8 +122,11 @@ $(function() {
     canvas: canvas,
     context: context,
     mapSprite: mapSprite,
+    objectSprite: objectSprite,
 
     bgTileMap: mapArray,
+
+    objTileMap: objectArray,
 
     mapDimensions: {
       fullWidth: mapArray.length,
@@ -108,6 +150,12 @@ $(function() {
       {'x': 252, 'y': 0}
     ],
 
+    objectMapSprites: [
+      {'x': 0, 'y': 0},
+      {'x': 50, 'y': 0},
+      {'x': 100, 'y': 0}
+    ],
+
     mapWidth: mapWidth,
     mapHeight: mapHeight,
     tileWidth: tileWidth,
@@ -125,6 +173,7 @@ $(function() {
       for(var i=0; i < this.mapDimensions.fullHeight - this.mapDimensions.yOffset; i++) {
         for(var j=0; j < this.mapDimensions.fullWidth - this.mapDimensions.xOffset; j++) {
           this.drawTile(j, i);
+          this.drawObject(j,i);
         }
       }
     },
@@ -143,11 +192,46 @@ $(function() {
         this.tileHeight);
     },
 
+    drawObject: function(x, y) {
+      var objectIndex = this.objTileMap[x + this.mapDimensions.xOffset][y + this.mapDimensions.yOffset] - 1;
+      if(objectIndex >= 0) {
+        var sourceCoords = this.objectMapSprites[objectIndex];
+        this.context.drawImage(
+          this.objectSprite,
+          sourceCoords.x,
+          sourceCoords.y,
+          this.tileWidth,
+          this.tileHeight,
+          (x * this.tileWidth),
+          (y * (this.tileHeight/2)),
+          this.tileWidth,
+          this.tileHeight);
+      }
+    },
+
     drawPlayer: function(){
       this.context.fillRect(player.x * this.tileWidth, (player.y * (this.tileHeight / 2)) + (this.tileHeight / 2), 20, 20);
     },
 
+    causesCollision: function(keycode) {
+      switch(keycode) {
+        case 37:
+          return (this.bgTileMap[player.globalX - 1][player.globalY] == 5 || this.objTileMap[player.globalX - 1][player.globalY] > 0);
+          break;
+        case 38:
+          return (this.bgTileMap[player.globalX][player.globalY - 1] == 5 || this.objTileMap[player.globalX][player.globalY - 1] > 0);
+          break;
+        case 39:
+          return (this.bgTileMap[player.globalX + 1][player.globalY] == 5 || this.objTileMap[player.globalX + 1][player.globalY] > 0);
+          break;
+        case 40:
+          return (this.bgTileMap[player.globalX][player.globalY + 1] == 5 || this.objTileMap[player.globalX][player.globalY + 1] > 0);
+          break;
+      }
+    },
+
     move: function(keycode) {
+      $('.speech-bubble').remove();
       centerTile = {
         x: Math.floor((canvas.width/tileWidth)/2),
         y: Math.floor((canvas.height/tileHeight)/2)
@@ -158,6 +242,7 @@ $(function() {
         39: centerTile.x + (centerTile.x/2), // righty-o
         40: centerTile.y + (centerTile.y/2)  // down town
       };
+      var collision = false;
 
       switch(keycode) {
         case 37:
@@ -167,15 +252,24 @@ $(function() {
             // play bump sound or something
             console.log("oww there is a wall there");
           }
-          else if(player.x < this.mapDimensions.movementAreaLeft && moMap.left) {
-            this.mapDimensions.xOffset--;
-            player.globalX--;
-            moMap.left = this.mapDimensions.xOffset > 0;
-            moMap.right = this.mapDimensions.xOffset + this.mapDimensions.visibleWidth < this.mapDimensions.fullWidth;
+          else if(this.causesCollision(keycode)){
+            collision = true;
+            collisionX = player.globalX - 1;
+            collisionY = player.globalY;
+            indeX = player.x - 1;
+            indeY = player.y;
           }
           else {
-            player.x--;
-            player.globalX--;
+            if(player.x < this.mapDimensions.movementAreaLeft && moMap.left) {
+              this.mapDimensions.xOffset--;
+              player.globalX--;
+              moMap.left = this.mapDimensions.xOffset > 0;
+              moMap.right = this.mapDimensions.xOffset + this.mapDimensions.visibleWidth < this.mapDimensions.fullWidth;
+            }
+            else {
+              player.x--;
+              player.globalX--;
+            }
           }
           break;
         case 38:
@@ -185,15 +279,24 @@ $(function() {
             // play bump sound or something
             console.log("oww there is a wall there");
           }
-          else if(player.y < this.mapDimensions.movementAreaUp && moMap.up) {
-            this.mapDimensions.yOffset--;
-            player.globalY--;
-            moMap.up = this.mapDimensions.yOffset > 0;
-            moMap.down = this.mapDimensions.yOffset + this.mapDimensions.visibleHeight < this.mapDimensions.fullHeight;
+          else if(this.causesCollision(keycode)){
+            collision = true;
+            collisionX = player.globalX;
+            collisionY = player.globalY - 1;
+            indeX = player.x;
+            indeY = player.y - 1;
           }
-          else if(player.y){
-            player.y--;
-            player.globalY--;
+          else {
+            if(player.y < this.mapDimensions.movementAreaUp && moMap.up) {
+              this.mapDimensions.yOffset--;
+              player.globalY--;
+              moMap.up = this.mapDimensions.yOffset > 0;
+              moMap.down = this.mapDimensions.yOffset + this.mapDimensions.visibleHeight < this.mapDimensions.fullHeight;
+            }
+            else if(player.y){
+              player.y--;
+              player.globalY--;
+            }
           }
           break;
 
@@ -203,16 +306,24 @@ $(function() {
             player.globalX = this.mapDimensions.fullWidth - 1;
             console.log("oww there is a wall there");
           }
-          else if(player.x > this.mapDimensions.movementAreaRight && moMap.right) {
-            this.mapDimensions.xOffset++;
-            player.globalX++;
-            moMap.left = this.mapDimensions.xOffset > 0;
-            moMap.right = this.mapDimensions.xOffset + this.mapDimensions.visibleWidth < this.mapDimensions.fullWidth;
+          else if(this.causesCollision(keycode)){
+            collision = true;
+            collisionX = player.globalX + 1;
+            collisionY = player.globalY;
+            indeX = player.x + 1;
+            indeY = player.y;
           }
           else {
-            player.x++;
-            player.globalX++;
-            console.log("movin playa right");
+            if(player.x > this.mapDimensions.movementAreaRight && moMap.right) {
+              this.mapDimensions.xOffset++;
+              player.globalX++;
+              moMap.left = this.mapDimensions.xOffset > 0;
+              moMap.right = this.mapDimensions.xOffset + this.mapDimensions.visibleWidth < this.mapDimensions.fullWidth;
+            }
+            else {
+              player.x++;
+              player.globalX++;
+            }
           }
           break;
         case 40:
@@ -221,19 +332,58 @@ $(function() {
             player.globalY = this.mapDimensions.fullHeight - 1;
             console.log("wall");
           }
-          else if(player.y > this.mapDimensions.movementAreaDown && moMap.down) {
-            this.mapDimensions.yOffset++;
-            player.globalY++;
-            moMap.up = this.mapDimensions.yOffset > 0;
-            moMap.down = this.mapDimensions.yOffset + this.mapDimensions.visibleHeight < this.mapDimensions.fullHeight;
+          else if(this.causesCollision(keycode)){
+            collision = true;
+            collisionX = player.globalX;
+            collisionY = player.globalY + 1;
+            indeX = player.x;
+            indeY = player.y + 1;
           }
           else {
-            player.y++;
-            player.globalY++;
+            if(player.y > this.mapDimensions.movementAreaDown && moMap.down) {
+              this.mapDimensions.yOffset++;
+              player.globalY++;
+              moMap.up = this.mapDimensions.yOffset > 0;
+              moMap.down = this.mapDimensions.yOffset + this.mapDimensions.visibleHeight < this.mapDimensions.fullHeight;
+            }
+            else {
+              player.y++;
+              player.globalY++;
+            }
           }
           break;
       }
       this.redraw();
+      if(collision) {
+        this.collisionHandling(indeX, indeY, collisionX, collisionY);
+      }
+    },
+
+    collisionHandling: function(indeX, indeY, x, y) {
+      $('.speech-bubble').remove();
+      var bubble = document.createElement("div");
+
+      switch(this.objTileMap[x][y]){
+        case 1:
+          bubble.innerHTML = "ya like climbing trees?";
+          //this.context.fillStyle = 'black';
+          //this.context.fillText("ya like climbing trees?", indeX * tileWidth, indeY * (tileHeight / 2));
+          break;
+        case 2:
+          bubble.innerHTML = "im a bush im a bush im a BUSH!";
+          //this.context.fillStyle = 'black';
+          //this.context.fillText("im a bush im a bush im a BUSH!", indeX * tileWidth, indeY * (tileHeight / 2));
+          break;
+        case 3:
+          bubble.innerHTML = "excuse me, rock here.";
+          //this.context.fillStyle = 'black';
+          //this.context.fillText("excuse me, rock here.", indeX * tileWidth, indeY * (tileHeight / 2));
+          break;
+      }
+      bubble.setAttribute("class", "speech-bubble");
+      $('#content').append(bubble);
+      $('.speech-bubble').css('top', (indeY * (tileHeight / 2) + 7)+ 'px');
+      $('.speech-bubble').css('left', (indeX * tileWidth + tileWidth + 10) + 'px');
     },
 
     moveMap: function() {
@@ -248,7 +398,6 @@ $(function() {
     footer: 57,
 
     resizeCanvas: function(e) {
-      console.log("resizing");
       canvas.width = $(window).innerWidth() - (2 * this.border);
       canvas.height = $(window).innerHeight() - this.footer;
       pample.gameBoard.mapDimensions = {
